d8a38b60c7ebb114a76c69a44ffd24c2d191b122
Son.HH(VJP) <son.hohai@vti.com.vn>
2019-05-16 17:28:52 +0900

    Initial Commit

---
+++ DESIGN_PATERN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DESIGN_PATERN
+//
+//  Created by VTI Japan on 2019/05/16.
+//  Copyright © 2019 VTI Japan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
